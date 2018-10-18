9eb16abc7a49509c5deacc4a60050b4203065ed5
Kjell-Ivar Johansen <johkje@elektro.no>
2018-10-18 18:19:36 +0200

    Init commit server

---
+++ server.js
@@ -0,0 +1,23 @@
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, (err) =>{
+    if(err){
+        console.log(err);
+        process.exit(1);
+    }
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+let port = process.env.PORT || 3000;
+
+app.get('/', (req,res)=>{
+    res.json({"message": "Welcome to retrogame api"})
+});
+
+app.listen(port);
+console.log("Listening on port: " + port);
